7fbcfc06c18269fd3822a65fcddb0d81104660b9
Bruce Tate <bruce@grox.io>
2023-11-01 12:08:53 -0400

    add rotate to points

---
+++ lib/group.ex
@@ -0,0 +1,41 @@
+defmodule Group do
+  def move_down(points) do
+    points
+  end
+
+  def move_left(points) do
+    points
+  end
+
+  def move_right(points) do
+    points
+  end
+
+  def swap(points) do
+    points
+  end
+
+  def flip_left_right(points) do
+    points
+  end
+
+  def flip_top_bottom(points) do
+    points
+  end
+
+  def rotate(points, 0) do
+    points
+  end
+
+  def rotate(points, 90) do
+    points
+  end
+
+  def rotate(points, 180) do
+    points
+  end
+
+  def rotate(points, 270) do
+    points
+  end
+end
