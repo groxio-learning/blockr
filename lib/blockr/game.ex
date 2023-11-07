6fdc2d1e5647e4a35dd23756aca9ffe1f6ef0bcf
Bruce Tate <bruce@grox.io>
2023-11-07 13:52:38 -0500

    move in game

--- lib/blockr/game.ex
+++ lib/blockr/game.ex
@@ -1,3 +1,32 @@
 defmodule Blockr.Game do
-  # functions that support game play
+  alias Blockr.Game.Tetromino
+
+  def left(board) do
+    tetro = Tetromino.left(board.tetro)
+    attempt(board, tetro)
+  end
+  def right(board) do
+    tetro = Tetromino.right(board.tetro)
+    attempt(board, tetro)
+  end
+  def turn(board) do
+    tetro = Tetromino.rotate_right_90(board.tetro)
+    attempt(board, tetro)
+  end
+
+  defp attempt(board, tetro) do
+    set =
+      tetro
+      |> Tetromino.to_group()
+      |> MapSet.new()
+
+    intersection = MapSet.intersection(set, board.points)
+    safe = MapSet.size(intersection) == 0
+
+    if safe do
+      %{board| tetro: tetro}
+    else
+      board
+    end
+  end
 end
